47b05c5d3418f1d925c93f82e0de11d50680d122
MAZeferino <alemaogameplay1@gmail.com>
2024-11-05 14:48:50 -0300

    Criando entidade categoria

--- src/main/java/com/udemynelio/course/config/TestConfig.java
+++ src/main/java/com/udemynelio/course/config/TestConfig.java
@@ -2,9 +2,11 @@ package com.udemynelio.course.config;
 import java.time.Instant;
 import java.util.Arrays;
 
+import com.udemynelio.course.entities.Category;
 import com.udemynelio.course.entities.Order;
 import com.udemynelio.course.entities.User;
 import com.udemynelio.course.entities.enums.OrderStatus;
+import com.udemynelio.course.repositories.CategoryRepository;
 import com.udemynelio.course.repositories.OrderRepository;
 import com.udemynelio.course.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,9 +21,17 @@ public class TestConfig implements CommandLineRunner {
     private UserRepository userRepository;
     @Autowired
     private OrderRepository orderRepository;
+    @Autowired
+    private CategoryRepository categoryRepository;
 
     @Override
     public void run(String... args) throws Exception {
+
+        Category cat1 = new Category(null, "Eletronic");
+        Category cat2 = new Category(null, "Books");
+        Category cat3 = new Category(null, "Cards");
+        categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+
         User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
         User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
         userRepository.saveAll(Arrays.asList(u1, u2));
